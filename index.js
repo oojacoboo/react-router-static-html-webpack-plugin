4aeb1152429d037b4a1d4712800c134431556925
Jacob Thomason <jacob@thomason.xxx>
2016-10-14 22:38:27 -0400

    Added ignoreExtensions arg to ignore require assets that shouldn't be compiled by typescript and additional cleanup

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//var fs = require('fs');
-
 var Q = require('q');
 var evaluate = require('eval');
 var path = require('path');
@@ -17,17 +15,17 @@ var reactRouterToArray = require('react-router-to-array');
  *
  * License (MIT) https://github.com/qimingweng/static-render-webpack-plugin
  *
- * @param {string} rootPath         The absolute root path of the project
- * @param {string} bundlePath       The path to the main js bundle generated from webpack
- * @param {object} reactRoutes      Path to the React Router JSX Routes
- * @param {object} [props={}]       Initial props is an object passed into the render function
- * @param {array} [watchFiles=[]]   An array of file paths to keep an eye on for changes
+ * @param {String} bundlePath             The path to the main js bundle generated from webpack
+ * @param {Object} reactRoutesPath        Path to the React Router JSX Routes
+ * @param {Array} [ignoreExtensions=[]]   Extensions to ignore and not try and compile
+ * @param {Object} [props={}]             Initial props is an object passed into the render function
+ * @param {Array} [watchFiles=[]]         An array of file paths to keep an eye on for changes
  * @constructor
  */
-function StaticRenderWebpackPlugin(rootPath, bundlePath, reactRoutesPath, props, watchFiles) {
-  this.rootPath = rootPath
+function StaticRenderWebpackPlugin(bundlePath, reactRoutesPath, ignoreExtensions, props, watchFiles) {
   this.bundlePath = bundlePath;
   this.reactRoutesPath = reactRoutesPath;
+  this.ignoreExtensions = ignoreExtensions || [];
   this.props = props || {};
   this.watchFiles = watchFiles || [];
 }
@@ -36,49 +34,33 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
 
   compiler.plugin('after-compile', function(compiler, done) {
-    // Keep an eye on these file paths, and recompile in watch mode if they change
-    if (Array.isArray(self.watchFiles)) {
+    //keep an eye on these file paths, and recompile in watch mode if they change
+    if(Array.isArray(self.watchFiles)) {
       self.watchFiles.forEach(function(src) {
         compiler.fileDependencies.push(src);
       });
     }
 
     require('ts-node').register({
-      project: self.rootPath,
       ignoreWarnings: [],
       disableWarnings: false,
-      compilerOptions: {
-        rootDir: self.rootPath
-      },
       fast: true,
       lazy: true
     });
 
     var sourceAsset = compiler.assets[self.bundlePath];
 
-//    require.extensions['.png'] = function(){};
-//    require.extensions['.scss'] = function(){};
-//    require.extensions['.gif'] = function(){};
-//    require.extensions['.jpg'] = function(){};
-//    require.extensions['.jpeg'] = function(){};
+    //suppress require processing for the ignoreExtensions
+    self.ignoreExtensions.forEach(function(ext) {
+      require.extensions[ext] = function(){};
+    });
 
     var Routing = require(self.reactRoutesPath);
-    console.log(Routing.default);
-
     var outputRules = reactRouterToArray(Routing.default);
 
-    if (sourceAsset) {
+    if(sourceAsset) {
       try {
         var source = sourceAsset.source(); //string content of the bundle
-//        var routes = []; //string content of Routes overridden below
-//        fs.readFile(self.reactRoutesPath, 'utf8', function(err, data) {
-//          if(err) {
-//            compiler.errors.push(err);
-//          }
-//
-//          var routes = data;
-//          console.log(routes);
-//        });
 
         //the source file is expected to return a module function by default
         //this function takes two parameters, a local and a callback
@@ -86,9 +68,6 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
         //using evaluate to retrieve the exported function from the source/routes files
         //https://github.com/pierrec/node-eval
         var render = evaluate(source, self.bundlePath, undefined, true);
-//        var outputRules = reactRouterToArray(
-//          evaluate(routes, self.bundlePath, undefined, true)
-//        );
 
         var renderPromises = outputRules.map(function(outputRule) {
           var renderPath = getInputPath(outputRule);
@@ -98,7 +77,7 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
             //props is either an object or a function which returns a value
             var props = self.props;
 
-            if (typeof props == 'function') {
+            if(typeof props == 'function') {
               props = props();
             }
 
@@ -106,10 +85,10 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
               resolve(htmlString);
             });
           }).then(function(result) {
-            // Save the new file created
+            //save the new file created
             compiler.assets[outputFilePath] = createAssetFromContents(result);
           }).fail(function(error) {
-            // Catch errors here and print them in webpack's error handler, without stopping webpack-dev-server
+            //catch errors here and print them in webpack's error handler, without stopping webpack-dev-server
             compiler.errors.push(error);
           });
         });
@@ -117,7 +96,7 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
         Q.all(renderPromises).then(function() {
           done();
         });
-      } catch (err) {
+      } catch(err) {
         //catch errors here and print them in webpack's error handler, without stopping webpack-dev-server
         compiler.errors.push(err.stack)
         done();
@@ -129,13 +108,13 @@ StaticRenderWebpackPlugin.prototype.apply = function(compiler) {
 }
 
 var getInputPath = function(outputRule) {
-  if (typeof outputRule == 'string') return outputRule;
-  if (typeof outputRule == 'object') return outputRule.path;
+  if(typeof outputRule == 'string') return outputRule;
+  if(typeof outputRule == 'object') return outputRule.path;
 }
 
 var getOutputPath = function(outputRule) {
-  if (typeof outputRule == 'string') return path.join(outputRule, '/index.html');
-  if (typeof outputRule == 'object') return outputRule.output;
+  if(typeof outputRule == 'string') return path.join(outputRule, '/index.html');
+  if(typeof outputRule == 'object') return outputRule.output;
 }
 
 var createAssetFromContents = function(contents) {
